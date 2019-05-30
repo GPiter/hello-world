08ed553ea7f0a3be10428c7de9d25e0346eec443
GPiter <GaninPeter@gmail.com>
2019-05-30 16:42:23 +0300

    Добавлена перегрузка операций + и -

--- My Class.cpp
+++ My Class.cpp
@@ -14,23 +14,37 @@ class Numbers {
 	}
 
 	void show_result() {
-		cout << "Число в квадрате: " << numb_1 << endl;
+		cout << "Результат: " << numb_1 << endl;
 }
 
-	void calculate(Numbers); // Прототип функции
+	void calculate(Numbers); // Прототип функции, возведение в квадрат
+	Numbers operator+(Numbers) const; // Прототип функции, перегрузка операции +
+	Numbers operator-(Numbers) const; // Прототип функции, перегрузка операции -
 };
 
+// Перегрузка операций + и -
+Numbers Numbers::operator+(Numbers number) const {
+	int num = numb_1 + number.numb_1;
+	return Numbers(num);
+}
 
+Numbers Numbers::operator-(Numbers number) const {
+	int num = numb_1 - number.numb_1;
+	return Numbers(num);
+}
+
+// Функция возведения в квадрат
 void Numbers::calculate(Numbers num1) {
-
 	numb_1 = num1.numb_1 * num1.numb_1;
-
 }
 
 int main() {
-	Numbers data1;
-	data1.get_number();
-	data1.calculate(data1);
-	data1.show_result();
+	Numbers data1(5);
+	Numbers data2, data3;
+	data2.get_number();
+	data3 = data1 + data2;
+	data3.show_result();
+	data3 = data1 - data2;
+	data3.show_result();
 	return 0;
 }
